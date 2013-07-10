eae9113f986505fe0ac294a07bca4c3812110652
Ian Eure <ian.eure@gmail.com>
2013-07-10 10:16:32 -0700

    A few updates.

--- nspace.el
+++ nspace.el
@@ -24,8 +24,14 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'cl))
+
 (defvar *ns* nil
   "The current namespace")
+
+(defconst nspace-separator "/"
+  "The string which separates the namespace prefix from the symbol.")
 
 (defun nspace-normalize (nsspec)
   "Turn NSSPEC into a namespace string."
@@ -33,28 +39,32 @@
         ((symbolp nsspec) (symbol-name nsspec))
         (t (error (format "Unknown nssspec `%s'" nsspec)))))
 
+(defun nspace-sym-in-scopep (nsspec sym)
+  "Is SYM in the scope of namespace NSSPEC?"
+  (string-match-p (concat "^" nsspec nspace-separator)
+                  (nspace-normalize sym)))
+
 (defun nspace-symbols (nsspec)
   "Return a list of pseudo-namespace symbols."
   (let ((syms)
         (prefix (nspace-normalize nsspec)))
     (mapatoms
      (lambda (sym)
-       (let ((name (symbol-name sym)))
-         (when (string-match-p (concat "^" prefix "/") name)
-           (push sym syms)))))
+       (when (nspace-sym-in-scopep prefix sym)
+         (push sym syms))))
     syms))
 
 (defun nspace-make-function-thunk (method)
   "Make a thunk macro for namespace NS method METHOD."
   (let* ((name (symbol-name method))
-         (ns (car (split-string name "/")))
+         (ns (car (split-string name nspace-separator)))
          (bare (intern (substring name (1+ (length ns))))))
     `(,bare (&rest args) ,(list 'backquote (list method ',@args)))))
 
 (defun nspace-make-symbol-thunk (sym)
   "Make a symbol-macrolet definition for namespace NS symbol SYM."
   (let* ((name (symbol-name sym))
-         (ns (car (split-string name "/")))
+         (ns (car (split-string name nspace-separator)))
          (bare (intern (substring name (1+ (length ns))))))
     (list bare sym)))
 
@@ -68,7 +78,7 @@
                   (remove-if-not 'fboundp syms)))))
 
 (defmacro with-ns (ns &rest body)
-  "Execute BODY in the context of namespace NS.
+  "Evaluate BODY in the context of namespace NS.
 
    Any function with a prefix of `NS/' will be aliased to its non
    nspace-qualified name while BODY is evaluated."
@@ -79,6 +89,7 @@
        (macrolet ,funcs
          (symbol-macrolet ,syms
            ,@body)))))
+(put 'with-ns 'lisp-indent-function 1)
 
 (provide 'nspace)
 ;;; nspace.el ends here
